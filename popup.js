e5ac35570884e6869b870c64bb0bc54b76d068f4
Irina-Komaritskaya <amsterdam.ira@yandex.ru>
2022-03-26 20:26:03 +0700

    create getData function

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+import { getData } from "./request";
 // получаем доступ к кнопке
 let snow = document.getElementById("snow");
 // когда кнопка нажата — находим активную вкладку и запускаем нужную функцию
@@ -15,15 +16,33 @@ snow.addEventListener("click", async () => {
 
 // запускаем снег
 function snowFall() {
+  async function getData(url) {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    });
+    let result = await response.text();
+    return result;
+  }
+
   const container = document.querySelector(
     '[data-automation="AssetGrids_MosaicAssetGrid_div"]'
   );
   const links = container.getElementsByTagName("a");
-  console.log(links[1].href);
- 
   for (let i = 0; i < links.length; i++) {
-    links[i].href;
-    
-    
+    const link = links[i].href;
+    getData(link).then((res) => {
+      const positionStart = res.indexOf(`{"props":`);
+      const positionFinish = res.lastIndexOf("</script>");
+      const str = res.slice(positionStart, positionFinish);
+      const data = JSON.parse(str);
+
+      const key = data.props.pageProps.asset.keywords;
+      console.log(key);
+    });
+
+    // let meetup = JSON.parse(str);
   }
 }
