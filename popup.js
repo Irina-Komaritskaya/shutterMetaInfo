8d921fd965c0ae766f7e28a68f044f8dc8ca8cd1
Irina-Komaritskaya <amsterdam.ira@yandex.ru>
2022-04-26 16:34:27 +0200

    position information

--- popup.js
+++ popup.js
@@ -44,38 +44,52 @@ function snowFall() {
       const positionFinish = res.lastIndexOf("</script>");
       const data = cutData(res, positionStart, positionFinish);
       const asset = data.props.pageProps.asset;
-     
-	  if (asset === undefined || asset === null) {
+
+      if (asset === undefined || asset === null) {
         return;
       }
-	  const keys = asset.keywords;
+      const keys = asset.keywords;
       if (keys === undefined || keys === null) {
         return;
       }
-     
-	  const item = links[i].parentElement; 
 
-	  const pin = document.createElement("button");
-	  pin.setAttribute("id", "pin");
-	  pin.textContent = "i";
-	  pin.style.position = "absolute";
-	  pin.style.bottom = "10px";
-	  pin.style.left = "10px";
-	  pin.style.width = "20px";
-	  pin.style.height = "20px";
-	  pin.style.backgroundColor = "#00BFFF";
-	  pin.style.borderRadius = '20px';
-	  pin.style.textAlign = "center";
-	  pin.style.fontSize = "15px"
-	  item.append(pin);
+      const item = links[i].parentElement;
 
-	  const pinHandler = (e) => {
-		e.stopPropagation();
-		  alert("1");
-	  } 
+      const pin = document.createElement("div");
+      pin.setAttribute("id", "pin");
+      pin.textContent = "i";
+      pin.style.position = "absolute";
+      pin.style.bottom = "10px";
+      pin.style.left = "10px";
+      pin.style.width = "20px";
+      pin.style.height = "20px";
+      pin.style.backgroundColor = "#00BFFF";
+      pin.style.borderRadius = "20px";
+      pin.style.textAlign = "center";
+      pin.style.fontSize = "15px";
+	  pin.style.zIndex = "10";
+	  pin.style.cursor = "pointer";
+      item.append(pin);
 
-	  pin.addEventListener("click", pinHandler);
-      
+      const pinHandler = (e) => {
+        e.stopPropagation();
+		
+        const information = document.createElement("div");
+        information.setAttribute("id", "information");
+		information.style.display = "flex";
+        information.style.width = "300px";
+        information.style.height = "200px";
+        information.style.backgroundColor = "#00BFFF";
+        information.style.position = "absolute";
+		information.style.zIndex = "10";
+		information.style.top = (e.pageY-230) + "px";
+		information.style.left = (e.pageX+30) + "px";
+        container.append(information);
+		
+      };
+
+      pin.addEventListener("click", pinHandler);
+
       const infoDiv = document.createElement("div");
       infoDiv.setAttribute("class", "infoContainer");
       infoDiv.textContent = keys;
