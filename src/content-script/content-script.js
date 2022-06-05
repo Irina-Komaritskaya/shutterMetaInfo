e03c3e1925c20e5e588abe299ccaa5461c6fb30b
Irina-Komaritskaya <amsterdam.ira@yandex.ru>
2022-06-05 19:56:03 +0200

    save keywords

--- src/content-script/content-script.js
+++ src/content-script/content-script.js
@@ -4,18 +4,13 @@ const container = document.querySelector(
     '[data-automation="AssetGrids_MosaicAssetGrid_div"]'
 );
 const links = container.getElementsByTagName("a");
-const keywordsSelected = {};
 
-const main = async () => {
-    for (let i = 0; i < links.length; i++) {
-        const item = links[i].parentElement;
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.setAttribute("id", `checkbox_${i}`);
-        checkbox.setAttribute("class", "checkbox");
-        item.append(checkbox);
-        item.addEventListener("change", checkboxHandler);
-    }
-};
-
-main();
+for (let i = 0; i < links.length; i++) {
+    const item = links[i].parentElement;
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.setAttribute("id", `checkbox_${i}`);
+    checkbox.setAttribute("class", "checkbox");
+    item.append(checkbox);
+    item.addEventListener("change", checkboxHandler);
+}
--- src/content-script/selected-image.js
+++ src/content-script/selected-image.js
@@ -1,21 +1,60 @@
 import { getMetaData } from "../request";
+
 export const checkboxHandler = (e) => {
+    const id = e.target.parentElement.getElementsByTagName("a")[0].href;
     if (e.target.checked) {
+        console.log("CHECED");
         isChecked(e).then((keywords) => {
-            console.log(keywords);
+            const [sorted, notSorted] = sortKeywords(keywords);
+
+            chrome.storage.local.get(["keywords"], function (result) {
+                let arr =
+                    Object.entries(result.keywords).length === 0
+                        ? []
+                        : result.keywords;
+                arr = [
+                    ...arr,
+                    {
+                        id: id,
+                        sellKeywords: notSorted,
+                        notSellKeywords: sorted,
+                    },
+                ];
+
+                chrome.storage.local.set({ keywords: arr });
+            });
         });
     } else {
-        console.log("uncheked");
+        chrome.storage.local.get(["keywords"], function (result) {
+            let arr = result.keywords;
+            const index = arr.findIndex((x) => x.id === id);
+            arr.splice(index, 1);
+            chrome.storage.local.set({ keywords: arr });
+        });
     }
 };
 
 const isChecked = async (e) => {
     const link = e.target.parentElement.getElementsByTagName("a")[0];
-    console.log(link);
     const metaData = await getMetaData(link);
     const keywords = metaData?.keywords;
     if (!metaData || !keywords) {
         return;
     }
     return keywords;
+};
+
+const sortKeywords = (arr) => {
+    let separator = null;
+
+    for (let i = arr.length - 1; i > 0; i--) {
+        if (arr[i - 1] > arr[i]) {
+            separator = i + 1;
+            break;
+        }
+    }
+    const sorted = arr.slice(separator, arr.length - 1);
+    const notSorted = arr.slice(0, separator);
+
+    return [sorted, notSorted];
 };
